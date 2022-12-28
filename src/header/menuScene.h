9e6bb542f8372b414b2ef95fe3f7a886d6d328d7
Abdessamade-OH <abdessamade.ohmad@etu.uae.ac.ma>
2022-12-28 11:00:21 +0100

    save

--- src/header/menuScene.h
+++ src/header/menuScene.h
@@ -1,11 +1,23 @@
-#ifndef menuScene.h
-#define menuScene.h
-#include "scene.h"
+#pragma once
+#include "gameHeader.h"
+#include <vector>
 
-class MenuScene : public Scene{
+class MenuScene {
+
+	public:
+		MenuScene();
+		~MenuScene();
+		
+		void update();
+		void render();
+		void handleEvents(); 
+		//int selected(){return selected}//returns selected scene
+		void clean();
+		//bool running(){return isRunning;}
 	
 	private:
-		
+		SDL_Texture* backgroundImage;
+		vector<MenuItem> menuItems;
+		//bool isRunning;
+		//int selected;
 };
-	
-#endif
